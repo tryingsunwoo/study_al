21da8f094b702107e01944af9fb3f0a914d2d2e9
tryingsunwoo <68853538+tryingsunwoo@users.noreply.github.com>
2020-08-09 18:28:46 +0900

    Add files via upload

---
+++ centerword.py
@@ -0,0 +1,23 @@
+def solution(s):
+    answer = ''
+
+    b = len(s) // 2
+
+    if len(s) % 2 == 0:
+        answer = s[b - 1] + s[b]
+    else:
+        answer = s[b]
+
+    return answer
+
+'''
+문제 설명
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+재한사항
+s는 길이가 1 이상, 100이하인 스트링입니다.
+입출력 예
+s	return
+abcde	c
+qwer	we
+'''
